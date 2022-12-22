b3f7cc972b9ccf61f898eff93bcc57ef0c05113e
Nicholas Page <npage@southhills.edu>
2022-12-21 21:28:11 -0500

    Initial commit

---
+++ IT100-Project1/IT100-Project1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+namespace IT100_Project1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
